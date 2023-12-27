4471e47cb38780f0dbea89c4a1be26f40255866c
Hristo Dimitrov <hristo.dimitrov@softwareone.com>
2023-12-27 18:20:13 +0200

    fix fmt and remove tfsec

--- terraform/environment/cloudtrail/s3.tf
+++ terraform/environment/cloudtrail/s3.tf
@@ -8,8 +8,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3" {
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
+    }
   }
-}
 }
 
 resource "aws_s3_bucket_policy" "cloudtrail_policy" {
