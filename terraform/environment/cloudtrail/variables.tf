543aa95937829fbcfcb318526148449df946cd42
Hristo Dimitrov <hristo.dimitrov@softwareone.com>
2023-12-27 17:34:25 +0200

    test tflint

---
+++ terraform/environment/cloudtrail/variables.tf
@@ -0,0 +1,17 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "my-cloud-trail-bucket-21-12-20231123322111111"
+}
+
+variable "trail_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "events"
+}
+
+variable "trail_name1" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "events"
+}
